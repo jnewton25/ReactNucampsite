21a3d201783f8fe018fbd1acf824673361ec6642
Justin Newton <justin-newton@outlook.com>
2022-03-05 12:21:02 -0600

    Task 3: Week 2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -23,11 +23,36 @@ class CampsiteInfo extends Component {
 		);
     }
 
+    renderComments(comments) {
+        if(comments) {
+            return (
+				<div className="col-md-5 m-1">
+					<h4>Comments</h4>
+					{comments.map((comments) => (
+						<div key={comments.id}>
+							{comments.text}
+							<br></br>
+							{"--"}{comments.author},{" "}
+							{new Intl.DateTimeFormat("en-US", {
+								year: "numeric",
+								month: "short",
+								day: "2-digit",
+							}).format(new Date(Date.parse(comments.date)))}
+						</div>
+					))}
+				</div>
+			);
+        } else {
+            return <div />;
+        }
+    }
+
 	render() {
 		if (this.props.campsite) {
 			return (
 				<div className="row">
 					{this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
 				</div>
 			);
 		} else {
