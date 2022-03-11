606bc8c91e7d988497ad6d5a512cfbcd66c61c71
Justin Newton <justin-newton@outlook.com>
2022-03-10 20:00:58 -0600

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -16,22 +16,50 @@ class Main extends Component {
 		super(props);
 		this.state = {
 			campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
+			comments: COMMENTS,
+			partners: PARTNERS,
+			promotions: PROMOTIONS,
 		};
 	}
 
 	render() {
-
-        const HomePage = () => {
+		const HomePage = () => {
 			return (
-                <Home 
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}
-                />
-            );
+				<Home
+					campsite={
+						this.state.campsites.filter(
+							(campsite) => campsite.featured
+						)[0]
+					}
+					promotion={
+						this.state.promotions.filter(
+							(promotion) => promotion.featured
+						)[0]
+					}
+					partner={
+						this.state.partners.filter(
+							(partner) => partner.featured
+						)[0]
+					}
+				/>
+			);
+		};
+
+		const CampsiteWithId = ({ match }) => {
+			return (
+				<CampsiteInfo
+					campsite={
+						this.state.campsites.filter(
+							(campsite) =>
+								campsite.id === +match.params.campsiteId
+						)[0]
+					}
+					comments={this.state.comments.filter(
+						(comment) =>
+							comment.campsiteId === +match.params.campsiteId
+					)}
+				/>
+			);
 		};
 
 		return (
@@ -46,7 +74,11 @@ class Main extends Component {
 							<Directory campsites={this.state.campsites} />
 						)}
 					/>
-                    <Route exact path="/contactus" component={Contact} />
+					<Route
+						path="/directory/:campsiteId"
+						component={CampsiteWithId}
+					/>
+					<Route exact path="/contactus" component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
