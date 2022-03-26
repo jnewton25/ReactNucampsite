262197301ec69bad203753da71a92de84a7bb5c7
Justin Newton <justin-newton@outlook.com>
2022-03-25 21:21:45 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -39,7 +39,7 @@ class CommentForm extends Component {
 
 	handleSubmit(values) {
 		this.toggleModal();
-        this.props.addComment(
+        this.props.postComment(
 			this.props.campsiteId,
 			values.rating,
 			values.author,
@@ -147,7 +147,7 @@ function RenderCampsite({ campsite }) {
 	);
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
 	if (comments) {
 		return (
 			<div className="col-md-5 m-1">
@@ -165,7 +165,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
 						}).format(new Date(Date.parse(comments.date)))}
 					</div>
 				))}
-				<CommentForm campsiteId={campsiteId} addComment={addComment} />
+				<CommentForm campsiteId={campsiteId} postComment={postComment} />
 			</div>
 		);
 	}
@@ -214,7 +214,7 @@ function CampsiteInfo(props) {
 					<RenderCampsite campsite={props.campsite} />
 					<RenderComments
 						comments={props.comments}
-						addComment={props.addComment}
+						postComment={props.postComment}
 						campsiteId={props.campsite.id}
 					/>
 				</div>
